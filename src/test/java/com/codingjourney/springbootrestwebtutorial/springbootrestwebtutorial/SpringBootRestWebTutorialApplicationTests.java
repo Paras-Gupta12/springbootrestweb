8d90976a50f77d2ca4f9e01e754ababc88df89ee
Paras-Gupta12 <parasgupta3062000@gmail.com>
2024-09-13 14:42:06 +0530

    Basic Structure of files in Spring Boot

---
+++ src/test/java/com/codingjourney/springbootrestwebtutorial/springbootrestwebtutorial/SpringBootRestWebTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingjourney.springbootrestwebtutorial.springbootrestwebtutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestWebTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
