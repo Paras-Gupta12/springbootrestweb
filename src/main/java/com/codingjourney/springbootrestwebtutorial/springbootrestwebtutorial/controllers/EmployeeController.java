01903a448f983ccf6c76822f2fe0852c19d58c00
Paras-Gupta12 <parasgupta3062000@gmail.com>
2024-09-13 16:36:00 +0530

    parent path set

--- src/main/java/com/codingjourney/springbootrestwebtutorial/springbootrestwebtutorial/controllers/EmployeeController.java
+++ src/main/java/com/codingjourney/springbootrestwebtutorial/springbootrestwebtutorial/controllers/EmployeeController.java
@@ -1,13 +1,12 @@
 package com.codingjourney.springbootrestwebtutorial.springbootrestwebtutorial.controllers;
 
 import com.codingjourney.springbootrestwebtutorial.springbootrestwebtutorial.dto.EmployeeDTO;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 
 @RestController
+@RequestMapping(path = "/employees")// for parent
 public class EmployeeController {
 
     //ComponentScan , it will tell the dispatcher servlet that all the mappings define here are the mappings you should the server
@@ -16,9 +15,11 @@ public class EmployeeController {
 //        return "$fswhdgus";
 //    }
 
-    @GetMapping(path = "/employees/{employeeID}")
+    @GetMapping(path = "/{employeeID}")
     public EmployeeDTO getEmployeeByID(@PathVariable Long employeeID){
         return new EmployeeDTO(employeeID, "Paras", "parasgupta3062000@gmail.com", 25 , LocalDate.of(2024, 01, 20), true);
     }
+
+
 
 }
